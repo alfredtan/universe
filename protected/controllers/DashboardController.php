bcc4cc97f33ad86d36a9020c8be57746c03d8865
Alfred Tan <alfredtph@gmail.com>
2012-03-08 22:48:51 +0800

    dashboard

---
+++ protected/controllers/DashboardController.php
@@ -0,0 +1,106 @@
+<?php
+
+class DashboardController extends Controller
+{
+	public $layout = '//layouts/dashboard_layout';
+	
+	
+	public function beforeAction($action)
+	{
+		header('P3P: CP="HONK"');
+		// forces yii to create session first
+		Yii::app()->session['time']=time();
+		return true;
+	}
+	
+	public function actionIndex()
+	{
+		// if time hasn't expired, redirect to users
+		$_SESSION['dashboard_last_activity'] =0;
+		
+		// if form post
+		if( isset($_POST['login']))
+		{
+			if($_POST['login'] == '!futurelab_nextdigital!')
+			{
+				$_SESSION['dashboard_last_activity'] = time();
+				$this->redirect('dashboard/users');
+			}
+		}
+		$this->render('index');
+	}
+	
+	public function actionUsers()
+	{
+		// echo time() . '<BR>';
+		// echo time() -  $_SESSION['dashboard_last_activity'] . '<BR>';
+		// echo  $_SESSION['dashboard_last_activity'];
+		// die();
+		if( time() - $_SESSION['dashboard_last_activity'] > 900 )
+		{
+			$this->redirect('/dashboard/index');
+		}
+		$_SESSION['dashboard_last_activity']=time();
+		
+		$users = Yii::app()->db->createCommand()
+			->select('u.fbid, u.name, u.nric, u.mobile, u.email, (u.dateRegistered + interval 8 hour) as dateRegistered, c.name as campusName')
+			->from('user u')
+			->join('campus c', 'u.campusId=c.id')
+			->where('u.email not in (select email from excludes)')
+			->order('dateRegistered asc')
+			->queryAll();
+
+		$this->render('users', array('users'=>$users));
+	}
+
+	public function actionUniverse()
+	{
+		if( time() - $_SESSION['dashboard_last_activity'] > 900 )
+		{
+			$this->redirect('/dashboard/index');
+		}
+		$_SESSION['dashboard_last_activity']=time();
+		// get users who answers correctly
+		// and dedup
+		// result is users who are eligible to win
+		$users = Yii::app()->db->createCommand()
+			->select('u.*, q.total')
+			->from('user u')
+			->join('quiz q', 'u.fbid=q.fbid')
+			->where('q.total=6 and u.email not in (select email from excludes)')
+			->order('u.dateRegistered asc')
+			->queryAll();
+		
+		$universe = Yii::app()->db->createCommand()
+			->select('u.fbid, u.name, u.nric, u.email, u.mobile , v.id as universeId, campus.name as campusName, course.name as courseName, interest.name as interestName, life.name as lifeName, v.courseId, v.courseId, v.interestId, v.lifeId, v.friend1, v.friend2, v.friend3, (v.dateCreate + interval 8 hour) as dateCreate ')
+			->from('universe v')
+			->leftJoin('user u', 'u.fbid=v.fbid')
+			->leftJoin('campus', 'v.campusId=campus.id')
+			->leftJoin('course', 'v.courseId=course.id')
+			->leftJoin('interest', 'v.interestId=interest.id')
+			->leftJoin('life', 'v.lifeId=life.id')
+			->group('u.fbid')
+			->order('dateCreate asc')
+			->queryAll();
+		
+		$this->render('universe', array('universe'=>$universe));
+		// foreach($universe as $u)
+		// {
+			// echo $u['universeId'] . ' asd asd as '  . $u['campusName'];
+		// }
+			// die();
+// 			
+// 			
+		// foreach($users as $user )
+		// {
+			// echo $user['name'] . ' and ' . $user['total'] . '<BR>';
+		// }
+		// die();
+		// $universe = Universe::model()->findAll('fbid=:fbid', array('fbid'=>$id));
+		
+//->where('email not in (select email from excludes)')
+
+		// $this->render('users', array('users'=>$users));
+	}
+	
+}
---
+++ protected/views/dashboard/index.php
@@ -0,0 +1,4 @@
+<form method="post" action="<?php echo Yii::app()->createUrl('dashboard/'); ?>">
+	<input type="text" id="login" name="login">
+	<input type="submit" value="Login">
+</form>
