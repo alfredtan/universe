5d59acccb2b6969da3523192f4216c414931228f
Alfred Tan <alfredtph@gmail.com>
2012-03-08 22:53:45 +0800

    change textfield to password

--- protected/views/dashboard/index.php
+++ protected/views/dashboard/index.php
@@ -1,4 +1,4 @@
 <form method="post" action="<?php echo Yii::app()->createUrl('dashboard/'); ?>">
-	<input type="text" id="login" name="login">
+	<input type="password" id="login" name="login">
 	<input type="submit" value="Login">
 </form>
